232b1c8aa91edebb2bf3b68929f63fef6896dbd1
Jeff Knupp <jeff@jeffknupp.com>
2013-07-22 23:19:55 -0400

    moar tests

--- sandman/test/test_sandman.py
+++ sandman/test/test_sandman.py
@@ -2,21 +2,52 @@
 
 from sandman import app, db
 import unittest
+import os
+import shutil
+import json
 
 class SandmanTestCase(unittest.TestCase):
 
+    DB_LOCATION = os.path.join(os.getcwd(), 'sandman', 'test', 'chinook')
     def setUp(self):
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/jknupp/code/github_code/sandman/sandman/test/chinook'
+        
+        if os.path.exists(self.DB_LOCATION):
+            os.unlink(self.DB_LOCATION)
+        shutil.copy(os.path.join(os.getcwd(), 'sandman', 'test', 'data', 'chinook'), self.DB_LOCATION) 
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + self.DB_LOCATION
         app.config['TESTING'] = True
         self.app = app.test_client()
         import models
 
     def tearDown(self):
-        pass
+        os.unlink(self.DB_LOCATION)
+        self.app = None
 
-    def test_get_artists(self):
+    def test_get(self):
         import json
         response = self.app.get('/artists')
         assert response.status_code == 200
         assert response.data
         assert len(json.loads(response.data)['resources']) == 275
+
+    def test_post(self):
+        response = self.app.post('/artists', 
+                content_type='application/json', 
+                data=json.dumps({'Name': 'Jeff Knupp'}))
+        assert response.status_code == 201
+        assert json.loads(response.data)['Name'] == 'Jeff Knupp'
+        self.assertEqual(json.loads(response.data)['links'], [{u'rel': u'self', u'uri': u'/artists/276'}])
+
+    def test_get_posted_resource(self):
+        post_response = self.app.post('/artists', 
+                content_type='application/json', 
+                data=json.dumps({'Name': 'Jeff Knupp'}))
+        location = post_response.headers['Location']
+        as_json = json.loads(post_response.data)
+        uri = as_json['links'][0]['uri']
+        response = self.app.get(location)
+        assert response.status_code == 200
+        response = self.app.get(uri)
+        assert response.status_code == 200
+        assert as_json['Name'] == 'Jeff Knupp'
+
