34c57613c276561260f9d85284d6953fd606a52d
Jeff Knupp <jknupp@appnexus.com>
2013-07-24 13:30:37 -0400

    add extension for HTTP documentation

--- conf.py
+++ conf.py
@@ -25,7 +25,12 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.doctest', 
+              'sphinx.ext.coverage', 
+              'sphinx.ext.viewcode',
+              'sphinxcontrib.httpdomain'
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
