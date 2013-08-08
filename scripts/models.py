44789d2ddfc28ac8149f6bda3f08d31eda9002c7
Jeff Knupp <jeff@jeffknupp.com>
2013-08-08 02:53:37 -0400

    Clean up directory structure, unused files, etc

---
+++ scripts/models.py
@@ -0,0 +1,15 @@
+from sandman.model import register, Model
+
+class Artist(Model):
+    __tablename__ = 'Artist'
+
+class Album(Model):
+    __tablename__ = 'Album'
+
+class Playlist(Model):
+    __tablename__ = 'Playlist'
+
+class Genre(Model):
+    __tablename__ = 'Genre'
+
+register((Artist, Album, Playlist, Genre))
