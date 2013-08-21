129e165b18b2e0635a4b8ea5f54a38c17d089955
Jeff Knupp <jeff@jeffknupp.com>
2013-08-21 05:56:20 -0400

    somewhat working admin

--- sandman/model/__init__.py
+++ sandman/model/__init__.py
@@ -6,8 +6,12 @@ from .models import Model
 from .. import db, app
 from flask import current_app
 from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
+from sqlalchemy.engine import reflection
+from sqlalchemy.orm import relationship
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqlamodel import ModelView
 
-def register(cls):
+def register(cls, use_admin=True):
     """Register with the API a :class:`sandman.model.Model` class and associated
     endpoint.
 
@@ -19,12 +23,38 @@ def register(cls):
     with app.app_context():
         if getattr(current_app, 'endpoint_classes', None) is None:
             current_app.endpoint_classes = {}
+            current_app.classes_by_name = {}
         if isinstance(cls, (list, tuple)):
             for entry in cls:
                 current_app.endpoint_classes[entry.endpoint()] = entry
+                current_app.classes.add(entry)
+                entry._use_admin = use_admin
         else:
             current_app.endpoint_classes[cls.endpoint()] = cls
+            current_app.classes.add(cls)
+            cls._use_admin = use_admin
     Model.prepare(db.engine)
+
+def prepare_relationships():
+    inspector = reflection.Inspector.from_engine(db.engine)
+    with app.app_context():
+        print 'preparing'
+        for cls in current_app.classes:
+            print 'got class'
+            for foreign_key in inspector.get_foreign_keys(cls.__tablename__):
+                print 'got foreign_key {} in table {}'.format(foreign_key['referred_table'], cls.__tablename__)
+                other = foreign_key['referred_table']
+                setattr(other, cls.__tablename__, relationship(cls.__tablename__, backref=foreign_key['referred_table']))
+                print type(getattr(other, cls.__tablename__))
+    print 'preparing'
+    Model.prepare(db.engine)
+
+def activate_admin_classes():
+    admin = Admin(app)
+    with app.app_context():
+        for cls in (cls for cls in current_app.classes if cls._use_admin == True):
+            admin.add_view(ModelView(cls, db.session))
+
 
 # Redefine 'Model' to be a sqlalchemy.ext.declarative.api.DeclarativeMeta
 # object which also derives from sandman.models.Model. The naming is done for
--- scripts/models.py
+++ scripts/models.py
@@ -1,18 +1,33 @@
-from sandman.model import register, Model
+from sandman.model import register, prepare_relationships, activate_admin_classes, Model
+from sqlalchemy.orm import relationship
 
 class Track(Model):
     __tablename__ = 'Track'
 
+    def __str__(self):
+        return self.Name
+
 class Artist(Model):
     __tablename__ = 'Artist'
+    def __str__(self):
+        return self.Name
 
 class Album(Model):
     __tablename__ = 'Album'
+    def __str__(self):
+        return self.Title
 
 class Playlist(Model):
     __tablename__ = 'Playlist'
+    def __str__(self):
+        return self.Name
 
 class Genre(Model):
     __tablename__ = 'Genre'
+    def __str__(self):
+        return self.Name
+
 
 register((Artist, Album, Playlist, Genre, Track))
+activate_admin_classes()
+prepare_relationships()
