4c395f8a4932a2220c3e1ad08a2ca251c2291dca
Jeff Knupp <jknupp@appnexus.com>
2013-12-24 09:49:12 -0500

    Fix linting errors; update pylintrc file

--- sandman/test/foreign_key_models.py
+++ sandman/test/foreign_key_models.py
@@ -1,9 +1,12 @@
+"""Test that foreign keys with non-trivial keys are properly ignored."""
 from sandman.model import register, activate, Model
 
 class JobSchedule(Model):
+    """JobSchedule table"""
     __tablename__ = 'job_schedule'
 
 class DataSources(Model):
+    """DataSources table"""
     __tablename__ = 'data_sources'
 
 register((JobSchedule, DataSources))
