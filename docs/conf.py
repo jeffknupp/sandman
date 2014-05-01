b6a3f81e9fee971a94d80a62ffb0c2d872fb2519
Jeff Knupp <jknupp@appnexus.com>
2014-05-01 14:34:26 -0400

    remove generated docs; add sphinx-rtd-theme requirement

--- docs/conf.py
+++ docs/conf.py
@@ -13,11 +13,13 @@
 # serve to show the default.
 
 import sys, os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -103,9 +105,8 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-# html_theme = 'nature'
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -113,7 +114,8 @@ pygments_style = 'sphinx'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
                       'Flask-Admin>=1.0.6',
                       'Flask-HTTPAuth>=2.2.1',
                       'docopt>=0.6.1',
+                      'sphinx-rtd-theme',
                       ],
     cmdclass={'test': PyTest},
     author_email='jeff@jeffknupp.com',
